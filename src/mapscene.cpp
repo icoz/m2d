47417f0eaa8976ea10e2a2650986d565008e442e
icoz <icoz.vt@gmail.com>
2012-04-30 11:06:53 +0400

    added MapScene::posToChunkCoords(QPointF pos)

--- src/mapscene.cpp
+++ src/mapscene.cpp
@@ -6,7 +6,30 @@ MapScene::MapScene(QString map_dir, QObject *parent)
     this->m_map_dir = map_dir;
 }
 
+void MapScene::setPlayerPos(QPointF pos)
+{
+    cur_pos = pos;
+
+}
+
 void MapScene::loadChunk(QString filename)
 {
 
 }
+
+QPoint MapScene::posToChunkCoords(QPointF pos)
+{
+    QPoint p;
+    p.setX(qint32(pos.x()) % (CHUNK_SIZE*TEXTURE_SIZE));
+    p.setY(qint32(pos.y()) % (CHUNK_SIZE*TEXTURE_SIZE));
+    return p;
+}
+
+//void MapScene::keyPressEvent(QKeyEvent *keyEvent)
+//{
+//    switch (keyEvent->key()){
+//    case Qt::Key_up:
+//        //move player up
+//        break;
+//    }
+//}
--- src/mapscene.h
+++ src/mapscene.h
@@ -9,14 +9,22 @@ class MapScene : public QGraphicsScene
 {
 public:
     MapScene(QString map_dir, QObject *parent = 0);
+    void setPlayerPos(QPointF pos);
 //    void loadMap();
 //    void saveMap();
 protected:
     void loadChunk(QString filename);
     void saveChunk(QString filename);
+
+    //virtual void keyPressEvent ( QKeyEvent * keyEvent );
+
 private:
     QString m_map_dir;
-    QMap<QPoint, Chunk*> chunks;
+    //QMap<QPoint, Chunk*> chunks;
+    Chunk* chunks[3][3];
+    QPointF cur_pos;
+
+    QPoint posToChunkCoords(QPointF pos);
 };
 
 #endif // MAPSCENE_H
